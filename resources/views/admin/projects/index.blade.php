b5734af348bc98eed9997e6d18e974d2890ad9f8
DarioEsposito89 <darioesposito.work@gmail.com>
2023-10-11 10:25:39 +0200

    modificato foreach

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -15,14 +15,14 @@
                         </tr>                    
                     </thead>
                     <tbody>
-                        @foreach ($projects as $singleProject )  
+                        @foreach ($projects as $specificProject )  
                         <tr>
-                            <td>{{$singleProject->title}}</td>
-                            <td>{{implode(",", $singleProject->language)}}</td>
-                            <td><img src={{ asset('/storage/' . $singleProject->thumb) }} class="img-thumbnail" style="width: 100px"></td>
-                            <td>{{$singleProject->link}}</td>
+                            <td>{{$specificProject->title}}</td>
+                            <td>{{implode(",", $specificProject->language)}}</td>
+                            <td><img src={{ asset('/storage/' . $specificProject->thumb) }} class="img-thumbnail" style="width: 100px"></td>
+                            <td>{{$specificProject->link}}</td>
                             <td>
-                                <a href="{{route("admin.projects.show", $singleProject->slug )}}" class="btn btn-primary btn-lg" type="button">Dettagli</a>
+                                <a href="{{route("admin.projects.show", $specificProject->slug )}}" class="btn btn-primary btn-lg" type="button">Dettagli</a>
                             </td>
                         </tr>
                         @endforeach
